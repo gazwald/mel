6aed12f4d9e7ac46ac47025b23a2ac014d02c9d6
gazwald <gary.brandon@gmail.com>
2019-12-29 15:05:04 +1100

    Remove bucket on destroy

--- deploy/deploy/deploy_stack.py
+++ deploy/deploy/deploy_stack.py
@@ -13,14 +13,14 @@ class DeployStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         domain = os.getenv('DOMAIN', 'gazwald.com')
-        sub_domain = "meltest." + domain
+        sub_domain = "meltest1." + domain
 
         certificate_id = 'c1636661-e3bb-497f-b057-8269a50796c8'
         arn = 'arn:aws:acm:us-east-1:{account}:certificate/{certificate_id}'.format(account=os.getenv('CDK_DEFAULT_ACCOUNT'),
                                                                                     certificate_id=certificate_id)
         certificate = certificatemanager.Certificate.from_certificate_arn(self, "Certificate", arn)
 
-        s3_bucket_source = s3.Bucket(self, "Bucket")
+        s3_bucket_source = s3.Bucket(self, "Bucket", removal_policy=core.RemovalPolicy.DESTROY)
 
         assets_directory = os.path.join(os.getcwd(), '..', 'src')
         s3deploy.BucketDeployment(self, "DeployWebsite",
