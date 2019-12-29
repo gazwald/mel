551c3186b5003f4b2a10fd22cb774fd2c65e0e90
gazwald <gary.brandon@gmail.com>
2019-12-29 11:38:58 +1100

    Adding default region

--- deploy/app.py
+++ deploy/app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+import os
 
 from aws_cdk import core
 
 from deploy.deploy_stack import DeployStack
 
+env = {'account': os.getenv('AWS_ACCOUNT', os.getenv('CDK_DEFAULT_ACCOUNT', '')),
+       'region': 'us-west-2'}
 
 app = core.App()
-DeployStack(app, "deploy")
+DeployStack(app, "deploy", env=env)
 
 app.synth()
